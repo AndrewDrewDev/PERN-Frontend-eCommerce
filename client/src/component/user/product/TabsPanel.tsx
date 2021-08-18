4e2bee2efd6dcb2320815b51f570cb16aa8b8dd9
oliver2070 <g2070d@gmail.com>
2021-08-18 20:11:05 +0300

    Add TabsPanel component to ProductPage

---
+++ client/src/component/user/product/TabsPanel.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from 'react'
+import { useState } from 'react'
+import { FC } from 'react'
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
+
+type TTabsPanel = {
+  description: string
+  info: string
+  paymentDelivery: string
+  reviews: string
+}
+
+const TabsPanel: FC<TTabsPanel> = (data): ReactElement => {
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const tabStyle: string =
+    'text-gray-600 py-4 px-6 block hover:text-blue-500 border-b-2 border-white focus:outline-none'
+
+  const selectedTabStyle = 'text-blue-500 font-medium border-blue-500'
+
+  return (
+    <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
+      <TabList className="md:flex text-center cursor-pointer bg-gray-50 rounded-lg">
+        <Tab selectedClassName={selectedTabStyle} className={tabStyle}>
+          Описание товара
+        </Tab>
+        <Tab selectedClassName={selectedTabStyle} className={tabStyle}>
+          Дополнительная иноформация
+        </Tab>
+        <Tab selectedClassName={selectedTabStyle} className={tabStyle}>
+          Оплата доставка
+        </Tab>
+        <Tab selectedClassName={selectedTabStyle} className={tabStyle}>
+          Отзывы
+        </Tab>
+      </TabList>
+      <div className="my-4 text-gray-600">
+        <TabPanel>{data.description}</TabPanel>
+        <TabPanel>{data.info}</TabPanel>
+        <TabPanel>{data.paymentDelivery}</TabPanel>
+        <TabPanel>{data.reviews}</TabPanel>
+      </div>
+    </Tabs>
+  )
+}
+
+export { TabsPanel }
--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -4,80 +4,93 @@ import { CategoryWrapper } from '../component/user/common/CategoryWrapper'
 import { Breadcrumb } from '../component/user/product/Breadcrumb'
 import { ProductInfo } from '../component/user/product/ProductInfo'
 import { ProductSlider } from '../component/user/product/ProductSlider'
+import { TabsPanel } from '../component/user/product/TabsPanel'
 import { mainPageStore } from '../store/MainPageStore'
 import { TFullProductData } from '../types'
 
 const ProductPage: FC = (): ReactElement => {
-    const { id }: { id: string } = useParams()
+  const { id }: { id: string } = useParams()
 
-    const data: TFullProductData = {
-        d691_exCategory1: 'Категория1',
-        d692_exCategory2: 'Категория2',
-        d693_exCategory3: 'Категория3',
-        d691_exCategory1EN: 'Kategoria1',
-        d692_exCategory2EN: 'Kategoria2',
-        d693_exCategory3EN: 'Kategoria3',
-        d720_exProductID: '0115-0101-00016',
-        d721_exProductName: 'Название товара',
-        d734_exProductNew: '',
-        d735_exProductDiscounts: '@',
-        d802_exPriceSell: '310.00',
-        d781_exEd: 'шт.',
-        d723_exProductDescription: 'Описание товара',
-        d748_exProductAmountRemaind: '',
-        d722_exProductInStock: '?',
-        d747_exProductCodeVender: 'G111',
-        d803_exPriceOldSell: '400.00',
-        d738_exProductManufacturer: 'Tupeprlksfdg;jksgfdk',
-        REST_imgUrl: {
-            preview:
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-0m.jpg',
-            other: [
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-1b.jpg',
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-2b.jpg',
-                'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-1b.jpg',
-                'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-2b.jpg',
-                'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-3b.jpg',
-                'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-4b.jpg',
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-1b.jpg',
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-2b.jpg',
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-3b.jpg',
-                'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-4b.jpg',
-            ],
-        },
-    }
+  const data: TFullProductData = {
+    d691_exCategory1: 'Категория1',
+    d692_exCategory2: 'Категория2',
+    d693_exCategory3: 'Категория3',
+    d691_exCategory1EN: 'Kategoria1',
+    d692_exCategory2EN: 'Kategoria2',
+    d693_exCategory3EN: 'Kategoria3',
+    d720_exProductID: '0115-0101-00016',
+    d721_exProductName: 'Название товара',
+    d734_exProductNew: '',
+    d735_exProductDiscounts: '@',
+    d802_exPriceSell: '310.00',
+    d781_exEd: 'шт.',
+    d723_exProductDescription: 'Описание товара',
+    d748_exProductAmountRemaind: '',
+    d722_exProductInStock: '?',
+    d747_exProductCodeVender: 'G111',
+    d803_exPriceOldSell: '400.00',
+    d738_exProductManufacturer: 'Tupeprlksfdg;jksgfdk',
+    REST_imgUrl: {
+      preview:
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-0m.jpg',
+      other: [
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-1b.jpg',
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/A03-2b.jpg',
+        'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-1b.jpg',
+        'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-2b.jpg',
+        'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-3b.jpg',
+        'https://siteup.com.ua/demo/msk/child-safety/img-product/790557-4b.jpg',
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-1b.jpg',
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-2b.jpg',
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-3b.jpg',
+        'https://siteup.com.ua/demo/msk/tupperware/img-product/B19-4b.jpg',
+      ],
+    },
+  }
 
-    return (
-        <div className="container mx-auto">
-            <h1 className="text-gray-700 text-3xl title-font font-medium mb-1">
-                {data.d721_exProductName}
-            </h1>
-            <Breadcrumb
-                category1={data.d691_exCategory1}
-                category1EN={data.d691_exCategory1EN}
-                category2={data.d692_exCategory2}
-                category2EN={data.d692_exCategory2EN}
-                category3={data.d693_exCategory3}
-                category3EN={data.d693_exCategory3EN}
-                finalName={data.d721_exProductName}
-            />
-            <div className="mx-auto flex flex-wrap">
-                <ProductSlider images={data.REST_imgUrl.other} />
-                <ProductInfo data={data} />
-            </div>
-            {/* <TabsPanel/> */}
-            <CategoryWrapper
-                name={'Акции'}
-                count={4}
-                products={mainPageStore.discountProducts}
-            />
-            <CategoryWrapper
-                name={'Новинки'}
-                count={4}
-                products={mainPageStore.newProducts}
-            />
-        </div>
-    )
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-gray-700 text-3xl title-font font-medium mb-1">
+        {data.d721_exProductName}
+      </h1>
+
+      <Breadcrumb
+        category1={data.d691_exCategory1}
+        category1EN={data.d691_exCategory1EN}
+        category2={data.d692_exCategory2}
+        category2EN={data.d692_exCategory2EN}
+        category3={data.d693_exCategory3}
+        category3EN={data.d693_exCategory3EN}
+        finalName={data.d721_exProductName}
+      />
+      <div className="mx-auto flex flex-wrap">
+        <ProductSlider images={data.REST_imgUrl.other} />
+        <ProductInfo data={data} />
+      </div>
+      <TabsPanel
+        description={data.d723_exProductDescription}
+        info={
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, fugit aliquid assumenda exercitationem doloremque vitae tempore tempora ex sequi! Eos soluta praesentium error neque nesciunt consectetur voluptates quidem. Fuga, laboriosam.'
+        }
+        paymentDelivery={
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, fugit aliquid assumenda exercitationem doloremque vitae tempore tempora ex sequi! Eos soluta praesentium error neque nesciunt consectetur voluptates quidem. Fuga, laboriosam.'
+        }
+        reviews={
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, fugit aliquid assumenda exercitationem doloremque vitae tempore tempora ex sequi! Eos soluta praesentium error neque nesciunt consectetur voluptates quidem. Fuga, laboriosam.'
+        }
+      />
+      <CategoryWrapper
+        name={'Акции'}
+        count={4}
+        products={mainPageStore.discountProducts}
+      />
+      <CategoryWrapper
+        name={'Новинки'}
+        count={4}
+        products={mainPageStore.newProducts}
+      />
+    </div>
+  )
 }
 
 export { ProductPage }
