ad5ad82d41bc7a600013eac3ce6228b14492a690
Andrew Dryuk <g2070d@gmail.com>
2021-10-19 22:24:40 +0300

    Refactor: Add link wrapper in link exist

--- src/component/user/common/CategoryWrapper.tsx
+++ src/component/user/common/CategoryWrapper.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react'
 import { TMainProductsData } from '../../../types'
 import { ProductCard } from './ProductCard'
 import { PageNotFound } from '../../../pages/PageNotFound'
+import { Link } from 'react-router-dom'
 
 type TCategoryWrapper = {
-  name: string | undefined
-  count: string | undefined
+  name: string
+  count: string
   products: TMainProductsData[] | null
   limit?: number
+  link?: string
 }
 
 const CategoryWrapper: FC<TCategoryWrapper> = ({
@@ -16,21 +18,46 @@ const CategoryWrapper: FC<TCategoryWrapper> = ({
   count,
   products,
   limit,
+  link,
 }): ReactElement => {
   if (products === null)
     return <PageNotFound title="Страница товаров не найдена" />
 
+  type TNameOfCategory = {
+    name: string
+    link?: string
+  }
+  const NameOfCategory: FC<TNameOfCategory> = ({ name, link }) => {
+    const LinkElem: FC = ({ children }) => {
+      if (!link) return <></>
+      return <Link to={link}>{children}</Link>
+    }
+
+    const CategoryName = () => {
+      return (
+        <h3 className="text-gray-700 text-2xl font-medium">
+          {name ? name : 'Название не определено'}
+        </h3>
+      )
+    }
+
+    if (link)
+      return (
+        <LinkElem>
+          <CategoryName />
+        </LinkElem>
+      )
+    return (
+      <>
+        <CategoryName />
+      </>
+    )
+  }
+
   return (
     <>
       <div className="py-2">
-        <a
-          href="https://siteup.com.ua/demo/msk/tupperware/discount"
-          className="hover:underline"
-        >
-          <h3 className="text-gray-700 text-2xl font-medium">
-            {name ? name : 'Название не определено'}
-          </h3>
-        </a>
+        <NameOfCategory name={name} link={link} />
         <span className="mt-3 text-sm text-gray-600">
           {count
             ? count + ' + Единиц товара'
@@ -39,7 +66,7 @@ const CategoryWrapper: FC<TCategoryWrapper> = ({
         <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {products.map((product, index) => {
             const count = ++index
-            if (limit && count > limit) return
+            if (limit && count > limit) return <></>
             return <ProductCard key={index} product={product} />
           })}
         </div>
