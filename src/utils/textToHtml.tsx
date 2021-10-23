2b3af004048893d1bc3a76d4f198bf48cfe469d8
Andrew Dryuk <g2070d@gmail.com>
2021-10-23 08:58:23 +0300

    Fix: Visual bug show text in description of products

--- src/utils/textToHtml.tsx
+++ src/utils/textToHtml.tsx
@@ -4,14 +4,17 @@ import reactStringReplace from 'react-string-replace'
 const textToHtml = (text: string): ReactNode => {
   let replacedText: string | ReactNodeArray
 
+  // Add latter to key unieque, else show visual bugs
   replacedText = reactStringReplace(text, /\*\*/gm, (match, i) => (
-    <span key={i} className="font-bold">
+    <span key={i + 'a'} className="font-bold">
       {match}
     </span>
   ))
 
-  replacedText = reactStringReplace(replacedText, /\n/gm, () => (
-    <div className="my-3"></div>
+  replacedText = reactStringReplace(replacedText, /\n/gms, (match, i) => (
+    <div key={i + 'b'} className="my-3">
+      {match}
+    </div>
   ))
 
   return replacedText
