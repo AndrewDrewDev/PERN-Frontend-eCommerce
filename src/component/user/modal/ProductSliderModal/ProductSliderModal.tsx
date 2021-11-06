312ad69e6929996a69386d229ab6a02ec2de1de8
Andrew Dryuk <g2070d@gmail.com>
2021-11-07 00:22:01 +0200

    Feat: animation for quick product slider

--- src/component/user/modal/ProductSliderModal/ProductSliderModal.tsx
+++ src/component/user/modal/ProductSliderModal/ProductSliderModal.tsx
@@ -8,11 +8,26 @@ import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 
 import './ProductSliderModal.css'
+import { useTransition, animated } from 'react-spring'
 
 const ProductSliderModal: FC = observer((): ReactElement => {
   const isShowing = modalStateStore.productSliderWidgetState.isShowing
   const productId = modalStateStore.productSliderWidgetState.productId
   const [images, setImages] = useState<TProductPageDataImages>()
+  const [destroyFromDOM, setDestroyFromDOM] = useState(false)
+
+  const transitionModal = useTransition(isShowing, {
+    from: { y: -50, opacity: 0 },
+    enter: { y: 0, opacity: 1 },
+    leave: { y: 50, opacity: 0 },
+    onDestroyed: () => setDestroyFromDOM(true),
+  })
+  const transitionBackground = useTransition(isShowing, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    onDestroyed: () => setDestroyFromDOM(true),
+  })
 
   useEffect(() => {
     if (productId) {
@@ -26,51 +41,65 @@ const ProductSliderModal: FC = observer((): ReactElement => {
       productId: '',
     })
 
-  if (isShowing && images)
+  if (destroyFromDOM && images)
     return (
-      <div
-        className="md:px-20 lg:px-32 xl:px-64 z-40 fixed w-screen h-screen
+      <>
+        {transitionBackground(
+          (style, item) =>
+            item && (
+              <animated.div
+                className="md:px-20 lg:px-32 xl:px-64 z-40 fixed w-screen h-screen
         inset-0 overflow-hidden flex justify-center items-center bg-black
         bg-opacity-75"
-        onClick={() => close()}
-      >
-        <div onClick={e => e.stopPropagation()}>
-          <Carousel emulateTouch={true} showStatus={false}>
-            {images.big.map((img, i) => (
-              <div
-                className="w-full flex justify-center"
-                style={{ maxHeight: '500px' }}
+                onClick={() => close()}
+                style={style}
               >
-                <img
-                  key={i}
-                  className=" items-center object-contain"
-                  src={REACT_API_URL + img}
-                  alt="Изображение товара"
-                />
-                <button
-                  className="mr-10 z-50 absolute top-0 right-0 bg-white text-red-600 border-2 rounded-full"
-                  onClick={() => close()}
-                >
-                  <svg
-                    className="w-10 h-10"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            ))}
-          </Carousel>
-        </div>
-      </div>
+                {transitionModal(
+                  (style, item) =>
+                    item && (
+                      <animated.div
+                        style={style}
+                        onClick={e => e.stopPropagation()}
+                      >
+                        <Carousel emulateTouch={true} showStatus={false}>
+                          {images.big.map((img, i) => (
+                            <div
+                              className="w-full flex justify-center"
+                              style={{ maxHeight: '500px' }}
+                            >
+                              <img
+                                key={i}
+                                className=" items-center object-contain"
+                                src={REACT_API_URL + img}
+                                alt="Изображение товара"
+                              />
+                              <button
+                                className="absolute mr-10 z-50 absolute top-0 right-0 bg-white rounded-full text-red-600"
+                                onClick={() => close()}
+                              >
+                                <svg
+                                  className="w-10 h-10"
+                                  fill="currentColor"
+                                  viewBox="0 0 20 20"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                    clipRule="evenodd"
+                                  />
+                                </svg>
+                              </button>
+                            </div>
+                          ))}
+                        </Carousel>
+                      </animated.div>
+                    )
+                )}
+              </animated.div>
+            )
+        )}
+      </>
     )
 
   return <></>
