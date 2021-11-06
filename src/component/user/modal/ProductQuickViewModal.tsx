a53c68847713403a9b6ba0e89e125b5c8d26322b
Andrew Dryuk <g2070d@gmail.com>
2021-11-06 22:29:29 +0200

    Feat: animation for ProductQuickViewModal.tsx

--- src/component/user/modal/ProductQuickViewModal.tsx
+++ src/component/user/modal/ProductQuickViewModal.tsx
@@ -5,73 +5,94 @@ import { TAddItemCartStore, TProductPageData } from '../../../types'
 import ProductApi from '../../../http/ProductApi'
 import { Breadcrumb } from '../product/Breadcrumb'
 import { ProductSlider } from '../product/ProductSlider'
-import ProductInfo from '../product/ProductInfo'
 import textToHtml from '../../../utils/textToHtml'
 import { ProductInStock } from '../product/ProductInStock'
-import { ProductOldPrice } from '../product/ProductOldPrice'
 import { ProductPrice } from '../product/ProductPrice'
 import { ProductCounter } from '../product/ProductCounter'
 import { cartStore } from '../../../store/CartStateStore'
 import { Link } from 'react-router-dom'
+import { useTransition, animated } from 'react-spring'
 
 const ProductQuickViewModal: FC = observer((): ReactElement => {
-  const [product, setProduct] = useState<TProductPageData | null | undefined>()
-
   const isShowing = modalStateStore.productQuickViewWidgetState.isShowing
   const productId = modalStateStore.productQuickViewWidgetState.productId
+
+  const [product, setProduct] = useState<TProductPageData | null | undefined>()
+  const [destroyFromDOM, setDestroyFromDOM] = useState(false)
+  const transitionModal = useTransition(isShowing, {
+    from: { y: -50, opacity: 0 },
+    enter: { y: 0, opacity: 1 },
+    leave: { y: 50, opacity: 0 },
+    onDestroyed: () => setDestroyFromDOM(true),
+  })
+  const transitionBackground = useTransition(isShowing, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    onDestroyed: () => setDestroyFromDOM(true),
+  })
 
   useEffect(() => {
     if (productId)
       ProductApi.fetchOneProduct(productId).then(data => setProduct(data))
   }, [productId])
 
-  if (isShowing && product)
+  if (destroyFromDOM && product)
     return (
       <>
-        <div
-          className="fixed inset-0 w-full h-screen flex justify-center
-          items-center z-50 bg-black bg-opacity-50 duration-300
-          overflow-y-hidden"
-          onClick={() => close()}
-        >
-          <div
-            className="relative text-gray-700 bg-white my-5 py-5 px-5 rounded w-11/12
-            md:w-4/5 overflow-auto"
-            onClick={e => e.stopPropagation()}
-            style={{
-              maxHeight: '95%',
-            }}
-          >
-            <button
-              className="absolute right-0 top-0 text-red-500 m-2 rounded-full focus:outline-none"
-              onClick={() => close()}
-            >
-              <svg
-                className="w-8 h-8 "
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+        {transitionBackground(
+          (style, item) =>
+            item && (
+              <animated.div
+                className="fixed inset-0 w-full h-screen flex justify-center
+                items-center z-50 bg-black bg-opacity-50 overflow-y-hidden"
+                onClick={() => close()}
+                style={style}
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            <div className="text-gray-700 text-3xl title-font font-medium my-1">
-              {product.name}
-            </div>
-            <Breadcrumb
-              categories={product.categories}
-              lastName={product.name}
-            />
-            <div className="mx-auto flex flex-wrap">
-              <ProductSlider images={product.images.big} />
-              <ProductInfoQuickView product={product} />
-            </div>
-          </div>
-        </div>
+                {transitionModal(
+                  (style, item) =>
+                    item && (
+                      <animated.div
+                        className="relative text-gray-700 bg-white my-5 py-5 px-5
+                  rounded w-11/12 md:w-4/5 overflow-auto"
+                        onClick={e => e.stopPropagation()}
+                        style={{ ...style, maxHeight: '95%' }}
+                      >
+                        <button
+                          className="absolute right-0 top-0 text-red-500 m-2
+                     rounded-full focus:outline-none"
+                          onClick={() => close()}
+                        >
+                          <svg
+                            className="w-8 h-8 "
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </button>
+                        <div className="text-gray-700 text-3xl title-font font-medium my-1">
+                          {product.name}
+                        </div>
+                        <Breadcrumb
+                          categories={product.categories}
+                          lastName={product.name}
+                        />
+                        <div className="mx-auto flex flex-wrap">
+                          <ProductSlider images={product.images.big} />
+                          <ProductInfoQuickView product={product} />
+                        </div>
+                      </animated.div>
+                    )
+                )}
+              </animated.div>
+            )
+        )}
       </>
     )
 
